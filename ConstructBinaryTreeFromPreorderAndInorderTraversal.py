115e5f7cd870dc5a0c6364b3f1a920f1796704d6
nie <niewenze0523@163.com>
2019-01-15 15:31:15 +0800

    fix error105

--- ConstructBinaryTreeFromPreorderAndInorderTraversal.py
+++ ConstructBinaryTreeFromPreorderAndInorderTraversal.py
@@ -1,4 +1,5 @@
-# Definition for a binary tree node.
+
+import bisect# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -6,38 +7,35 @@ class TreeNode:
         self.right = None
 
 class Solution:
-
     def buildTree(self, preorder, inorder):
         """
         :type preorder: List[int]
         :type inorder: List[int]
         :rtype: TreeNode
         """
+        dictA = {}
+        for i, v in enumerate(inorder):
+            dictA[v] = i
+
         def buildSubTree(preorder, inorder):
+
             if not preorder:
                 return None
             node = TreeNode(preorder[0])
             if len(preorder) == 1:
                 return node
             else:
-                leftCount = dictA[node.val] - dictB[node.val] + 1
-                node.left = buildSubTree(preorder[1:leftCount+1], inorder[:leftCount])
+                leftCount = dictA[node.val] - dictA[inorder[0]]
+                node.left = buildSubTree(preorder[1:  1 + leftCount], inorder[:leftCount])
                 node.right = buildSubTree(preorder[leftCount+1:], inorder[leftCount+1:])
-            return node 
-
-        dictA = {} 
-        dictB = {} 
-        for i, v in enumerate(inorder):
-            dictA[v] = i
-        for i, v in enumerate(preorder):
-            dictB[v] = i
-
+            return node
         retNode = buildSubTree(preorder, inorder)
-        return  retNode
-
+        return retNode
 
 S = Solution()
-a1 = [5,4,1,2,3]
-a2 = [1,2,3,4,5]
+#a1 = [5,4,1,2,3]
+#a2 = [1,2,3,4,5]
+a1 = [3,9,20,15,7]
+a2 = [9,3,15,20,7]
 node = S.buildTree(a1, a2)
 print(node.val)
